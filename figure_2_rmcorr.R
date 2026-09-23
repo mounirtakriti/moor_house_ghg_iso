357e10a7ee0562233ae3c7b5d52acc804da05f03
mounirtakriti <60823463+mounirtakriti@users.noreply.github.com>
2026-09-23 15:40:13 +0300

    Adjust axes limits; axes labels

--- figure_2_rmcorr.R
+++ figure_2_rmcorr.R
@@ -16,17 +16,18 @@ source("setup.R")
 
 ch4_temp <- plot_rmc(rmc_ihsCH4_temp) + 
   okabe_ito_custom +
-  ylim(-2, 12) +
+  ylim(-1, 6) +
   xlab("Soil temperature (°C)") +
-  ylab(expression(ihs(CH[4]))) +
+  ylab(expression(ihs(CH[4]~flux))) +
   labs(colour='Location')
 
 ch4_co2 <- plot_rmc(rmc_ihsCH4_ihsCO2) + 
   # chcol +
   okabe_ito_custom +
-  ylim(-2, 12) +
-  xlab(expression(ihs(CO[2]))) +
-  labs(colour='Location') 
+  ylim(-1, 6) +
+  xlab(expression(ihs(CO[2]~flux))) +
+  labs(colour='Location') +
+  theme(axis.title.y = element_blank())
 
 
 
@@ -36,6 +37,8 @@ pdf("Figure_2_rmcorr.pdf", width = 8, height = 4)
 ch4_temp + ch4_co2 + 
   plot_annotation(tag_levels = "a") + 
   plot_layout(guides = "collect",
-              axes = "collect") &  
+              # axes = "collect"
+              ) &  
   theme(legend.position = "bottom")
 dev.off()
+
